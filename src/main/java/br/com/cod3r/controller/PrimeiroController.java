533cea450cd70e77110bf5f3c01f8bbfdae18f41
daniel cooder <danielcooder@gmail.com>
2025-02-07 16:46:03 -0300

    passando parametros para web service

--- src/main/java/br/com/cod3r/controller/PrimeiroController.java
+++ src/main/java/br/com/cod3r/controller/PrimeiroController.java
@@ -1,7 +1,6 @@
 package br.com.cod3r.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
