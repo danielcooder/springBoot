9bc87fa3dfb732518695a1df5b672c36c8271009
daniel cooder <danielcooder@gmail.com>
2025-02-07 17:03:37 -0300

    passando parametros - incluindo defaultValue

--- src/main/java/br/com/cod3r/controller/ClienteController.java
+++ src/main/java/br/com/cod3r/controller/ClienteController.java
@@ -1,9 +1,6 @@
 package br.com.cod3r.controller;
 import br.com.cod3r.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
@@ -18,5 +15,12 @@ public class ClienteController {
     public Cliente obterClientePorId1(@PathVariable int id){
         return new Cliente(id, "Maria", "987.654.321-00");
     }
+
+    @GetMapping
+    public Cliente obterClientePorId2(
+            @RequestParam(name = "id", defaultValue = "1") int id){
+        return new Cliente(id, "João Augusto", "111.222.333-44");
+    }
+
 
 }
