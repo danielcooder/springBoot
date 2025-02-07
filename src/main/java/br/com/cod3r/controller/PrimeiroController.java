ee2774059f7a1093df70b544c510cf6e1a06ecdb
daniel cooder <danielcooder@gmail.com>
2025-02-07 14:51:54 -0300

    web-retornando objetos

--- src/main/java/br/com/cod3r/controller/PrimeiroController.java
+++ src/main/java/br/com/cod3r/controller/PrimeiroController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,5 @@ public class PrimeiroController {
     public String ola(){
         return "Olá Spring Boot";
     }
-
 
 }
